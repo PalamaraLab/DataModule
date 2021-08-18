5e69cfea8b04242d58ba7a7eac9caec7de8d46cd
Fergus Cooper <fcooper8472@gmail.com>
2021-08-18 18:55:09 +0100

    Rename export targets for consistency

--- cmake/asmc-data-module-config.cmake
+++ cmake/asmc-data-module-config.cmake
@@ -5,4 +5,4 @@ find_dependency(fmt)
 find_dependency(range-v3)
 find_dependency(ZLIB)
 
-include(${CMAKE_CURRENT_LIST_DIR}/AsmcDataModule_Runtime.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/asmc-data-module-runtime.cmake)
